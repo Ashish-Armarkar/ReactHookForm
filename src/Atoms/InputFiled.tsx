56a696f81f5aa2fd72a428588e395d43124c2b9a
Ashish Vinod Armarkar <ashishvinodarmarkar@Ashishs-MacBook-Air.local>
2025-09-23 21:50:39 +0530

    Input fields added

--- src/Atoms/InputFiled.tsx
+++ src/Atoms/InputFiled.tsx
@@ -1,13 +1,17 @@
-import { Flex, Input, Typography } from "antd";
 import { Controller, type Control } from "react-hook-form";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button, Input, Space, Flex, Typography } from "antd";
+import { useState } from "react";
+import NumericInput from "./NumberInput";
 
 interface InputFieldProps {
   placeholder: string;
-  control: Control<any>; // or Control<FormValues> if you have a type
+  control: Control<any>;
   required?: boolean;
   label: string;
   fieldKey: string;
   validation: {};
+  type: string;
 }
 
 const InputField = ({
@@ -17,7 +21,17 @@ const InputField = ({
   label,
   fieldKey,
   validation,
+  type,
 }: InputFieldProps) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [value, setValue] = useState("");
+  const { TextArea } = Input;
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    console.log(e);
+  };
   return (
     <Controller
       name={fieldKey}
@@ -29,18 +43,56 @@ const InputField = ({
       render={({ field, fieldState: { error } }) => (
         <>
           <Typography.Title level={5}>{label}</Typography.Title>
-          <Flex vertical gap={8}>
-            <Input
-              {...field}
-              placeholder={placeholder}
-              status={error ? "error" : ""} // AntD v5 shows red border
+          {type === "password" ? (
+            <Space direction="vertical">
+              <Input.Password placeholder="input password" />
+              <Input.Password
+                placeholder="input password"
+                iconRender={(visible) =>
+                  visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                }
+              />
+              <Space direction="horizontal">
+                <Input.Password
+                  placeholder="input password"
+                  visibilityToggle={{
+                    visible: passwordVisible,
+                    onVisibleChange: setPasswordVisible,
+                  }}
+                />
+                <Button
+                  style={{ width: 80 }}
+                  onClick={() => setPasswordVisible((prevState) => !prevState)}
+                >
+                  {passwordVisible ? "Hide" : "Show"}
+                </Button>
+              </Space>
+              <Input.Password disabled placeholder="disabled input password" />
+            </Space>
+          ) : type === "textarea" ? (
+            <TextArea
+              placeholder="textarea with clear icon"
+              allowClear
+              onChange={onChange}
             />
-            {error && (
-              <span style={{ color: "red", fontSize: 12 }}>
-                {error.message}
-              </span>
-            )}
-          </Flex>
+          ) : type === "number" ? (
+            <NumericInput
+              style={{ width: 120 }}
+              value={value}
+              onChange={setValue}
+            />
+          ) : (
+            <Flex vertical gap={8}>
+              <Input
+                {...field}
+                placeholder={placeholder}
+                status={error ? "error" : ""}
+              />
+            </Flex>
+          )}
+          {error && (
+            <span style={{ color: "red", fontSize: 12 }}>{error.message}</span>
+          )}
         </>
       )}
     />
---
+++ src/Atoms/RadioField.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Radio } from "antd";
+
+const RadioField: React.FC = () => <Radio>Radio</Radio>;
+
+export default RadioField;
